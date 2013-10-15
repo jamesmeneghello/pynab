940ce8af8366d3b50730e2e522bd83e91e1fb4b5
James Meneghello <murodese@gmail.com>
2013-10-15 23:41:18 +0800

    fixed path on two scripts

--- scripts/process.py
+++ scripts/process.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ''))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
 import pynab.binaries
 import pynab.releases
