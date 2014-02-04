5342f010bd1b9d4e6ba43896a7469692343a255a
James Meneghello <murodese@gmail.com>
2014-02-04 15:21:41 +0800

    added basic webui that allows simple querying by name/category using api key

---
+++ webui/app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('pynabWebuiApp', ['ui.router','angularMoment', 'ngCookies'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('index', {
+        url: '/',
+        views: {
+          'content': {templateUrl: 'views/index.html'}
+        }
+      })
+      .state('search', {
+        url: '/search',
+        views: {
+          'content': {templateUrl: 'views/search.html', controller: 'SearchCtrl'}
+        }
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html'
+      });
+  })
+  .run(['$rootScope', '$state', '$stateParams', function($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+  }]);
---
+++ webui/app/scripts/config.js
@@ -0,0 +1,6 @@
+angular.module('pynabWebuiApp').constant('PYNAB_CONFIG', {
+	// example: 'http://someindexer.org/'
+	// don't forget the trailing slash
+	// if your install is in a subdirectory, include that
+	hostUrl: 'http://murodese.dyndns.org/'
+});
