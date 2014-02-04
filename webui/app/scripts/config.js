bf3c0b04208c1deabd1c27b49cd4487323f70828
James Meneghello <murodese@gmail.com>
2014-02-04 16:22:59 +0800

    reset config file

--- webui/app/scripts/config.js
+++ webui/app/scripts/config.js
@@ -2,5 +2,5 @@ angular.module('pynabWebuiApp').constant('PYNAB_CONFIG', {
 	// example: 'http://someindexer.org/'
 	// don't forget the trailing slash
 	// if your install is in a subdirectory, include that
-	hostUrl: 'http://murodese.dyndns.org/'
+	hostUrl: ''
 });
