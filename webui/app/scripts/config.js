8a54a6b94a197fc0718ead1542454734b4acdce8
James Meneghello <murodese@gmail.com>
2014-02-15 14:15:42 +0800

    removed the webui temporarily so i can do a rebuild

--- webui/app/scripts/config.js
+++
@@ -1,6 +0,0 @@
-angular.module('pynabWebuiApp').constant('PYNAB_CONFIG', {
-	// example: 'http://someindexer.org/'
-	// don't forget the trailing slash
-	// if your install is in a subdirectory, include that
-	hostUrl: ''
-});
