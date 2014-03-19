f542458eb4fddd4ad7d259720567b744d2f17e09
James Meneghello <murodese@gmail.com>
2014-03-19 22:35:59 +0800

    make instructions for webui config slightly clearer

--- webui/app/scripts/config.js
+++ webui/app/scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('pynabWebuiApp').constant('PYNAB_CONFIG', {
-	// example: 'http://someindexer.org/'
+	// example: 'http://someindexer.org:8080/'
 	// don't forget the trailing slash
 	// if your install is in a subdirectory, include that
 	hostUrl: ''
