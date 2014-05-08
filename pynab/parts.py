7c5dca0e3a6d1663fd99a00478d1dee668d5e36e
James Meneghello <murodese@gmail.com>
2014-05-08 14:32:21 +0800

    more part-saving optimisations for certain scenarios

--- pynab/parts.py
+++ pynab/parts.py
@@ -1,115 +1,116 @@
 import regex
 import time
 import io
-import pytz
 
 from pynab.db import db_session, engine, Part, Segment
 from pynab import log
 
 from sqlalchemy.orm import Load, subqueryload
-import psycopg2
 
 
 def save_all(parts):
     """Save a set of parts to the DB, in a batch if possible."""
 
-    start = time.time()
-
-    with db_session() as db:
-        existing_parts = dict(
-            ((part.subject, part) for part in
-                db.query(Part.id, Part.subject).filter(Part.subject.in_(parts.keys())).all()
-            )
-        )
-
-        part_inserts = []
-        for subject, part in parts.items():
-            existing_part = existing_parts.get(subject, None)
-            if not existing_part:
-                segments = part.pop('segments')
-                part_inserts.append(part)
-                part['segments'] = segments
-
-        ordering = ['subject', 'group_name', 'posted', 'posted_by', 'total_segments', 'xref']
-
-        s = io.StringIO()
-        for part in part_inserts:
-            for item in ordering:
-                if item == 'posted':
-                    s.write(part[item].replace(tzinfo=None).strftime('%Y-%m-%d %H:%M:%S') + "\t")
-                elif item == 'xref':
-                    # leave off the tab
-                    s.write(part[item])
-                else:
-                    s.write(str(part[item]) + "\t")
-            s.write("\n")
-        s.seek(0)
-
-        conn = engine.raw_connection()
-        cur = conn.cursor()
+    if parts:
         start = time.time()
-        cur.copy_from(s, 'parts', columns=ordering)
-        conn.commit()
-        end = time.time()
-        log.debug('Time: {:.2f}s'.format(end - start))
 
-        #engine.execute(Part.__table__.insert(), part_inserts)
-
-        existing_parts = dict(
-            ((part.subject, part) for part in
-                db.query(Part)
-                .options(
-                    subqueryload('segments'),
-                    Load(Part).load_only(Part.id, Part.subject),
-                    Load(Segment).load_only(Segment.id, Segment.segment)
+        with db_session() as db:
+            existing_parts = dict(
+                ((part.subject, part) for part in
+                    db.query(Part.id, Part.subject).filter(Part.subject.in_(parts.keys())).all()
                 )
-                .filter(Part.subject.in_(parts.keys()))
-                .all()
             )
-        )
 
-        segment_inserts = []
-        for subject, part in parts.items():
-            existing_part = existing_parts.get(subject, None)
-            if existing_part:
-                segments = dict(((s.segment, s) for s in existing_part.segments))
-                for segment_number, segment in part['segments'].items():
-                    if int(segment_number) not in segments:
-                        segment['part_id'] = existing_part.id
-                        segment_inserts.append(segment)
-            else:
-                log.error('i\'ve made a huge mistake')
+            part_inserts = []
+            for subject, part in parts.items():
+                existing_part = existing_parts.get(subject, None)
+                if not existing_part:
+                    segments = part.pop('segments')
+                    part_inserts.append(part)
+                    part['segments'] = segments
 
-        ordering = ['segment', 'size', 'message_id', 'part_id']
+            if part_inserts:
+                ordering = ['subject', 'group_name', 'posted', 'posted_by', 'total_segments', 'xref']
 
-        s = io.StringIO()
-        for segment in segment_inserts:
-            for item in ordering:
-                if item == 'part_id':
-                    # leave off the tab
-                    s.write(str(segment[item]))
+                s = io.StringIO()
+                for part in part_inserts:
+                    for item in ordering:
+                        if item == 'posted':
+                            s.write(part[item].replace(tzinfo=None).strftime('%Y-%m-%d %H:%M:%S') + "\t")
+                        elif item == 'xref':
+                            # leave off the tab
+                            s.write(part[item])
+                        else:
+                            s.write(str(part[item]) + "\t")
+                    s.write("\n")
+                s.seek(0)
+
+                conn = engine.raw_connection()
+                cur = conn.cursor()
+                insert_start = time.time()
+                cur.copy_from(s, 'parts', columns=ordering)
+                conn.commit()
+                insert_end = time.time()
+                log.debug('Time: {:.2f}s'.format(insert_end - insert_start))
+
+                #engine.execute(Part.__table__.insert(), part_inserts)
+
+            existing_parts = dict(
+                ((part.subject, part) for part in
+                    db.query(Part)
+                    .options(
+                        subqueryload('segments'),
+                        Load(Part).load_only(Part.id, Part.subject),
+                        Load(Segment).load_only(Segment.id, Segment.segment)
+                    )
+                    .filter(Part.subject.in_(parts.keys()))
+                    .all()
+                )
+            )
+
+            segment_inserts = []
+            for subject, part in parts.items():
+                existing_part = existing_parts.get(subject, None)
+                if existing_part:
+                    segments = dict(((s.segment, s) for s in existing_part.segments))
+                    for segment_number, segment in part['segments'].items():
+                        if int(segment_number) not in segments:
+                            segment['part_id'] = existing_part.id
+                            segment_inserts.append(segment)
                 else:
-                    s.write(str(segment[item]) + "\t")
-            s.write("\n")
-        s.seek(0)
+                    log.error('i\'ve made a huge mistake')
 
-        conn = engine.raw_connection()
-        cur = conn.cursor()
-        insert_start = time.time()
-        cur.copy_from(s, 'segments', columns=ordering)
-        conn.commit()
-        insert_end = time.time()
-        log.debug('parts: postgres copy time: {:.2f}s'.format(insert_end - insert_start))
+            if segment_inserts:
+                ordering = ['segment', 'size', 'message_id', 'part_id']
 
-        #engine.execute(Segment.__table__.insert(), segment_inserts)
+                s = io.StringIO()
+                for segment in segment_inserts:
+                    for item in ordering:
+                        if item == 'part_id':
+                            # leave off the tab
+                            s.write(str(segment[item]))
+                        else:
+                            s.write(str(segment[item]) + "\t")
+                    s.write("\n")
+                s.seek(0)
 
-    end = time.time()
+                conn = engine.raw_connection()
+                cur = conn.cursor()
+                insert_start = time.time()
+                cur.copy_from(s, 'segments', columns=ordering)
+                conn.commit()
+                insert_end = time.time()
+                log.debug('parts: postgres copy time: {:.2f}s'.format(insert_end - insert_start))
 
-    log.debug('parts: saved {} parts and {} segments in {:.2f}s'.format(
-        len(part_inserts),
-        len(segment_inserts),
-        end - start
-    ))
+                #engine.execute(Segment.__table__.insert(), segment_inserts)
+
+        end = time.time()
+
+        log.debug('parts: saved {} parts and {} segments in {:.2f}s'.format(
+            len(part_inserts),
+            len(segment_inserts),
+            end - start
+        ))
 
 
 def is_blacklisted(subject, group_name, blacklists):
