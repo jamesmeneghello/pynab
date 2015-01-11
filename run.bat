9c22865854d661c73ddf6c7683abfcfcae191055
James Meneghello <murodese@gmail.com>
2015-01-11 11:44:06 +0800

    removed redundant files

--- run.bat
+++
@@ -1,7 +0,0 @@
-echo "Pynab started. You can use process manager to kill spawned processes (called python.exe)."
-echo "Make sure that your PATH has python set to a python3 directory."
-start python start.py
-start python postprocess.py
-python api.py
-pause
-exit
