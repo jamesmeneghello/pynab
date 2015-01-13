ab483635634dd3ebfc77a65b962b9ea3cbde0149
James Meneghello <murodese@gmail.com>
2015-01-13 18:54:50 +0800

    fix pathing problem in pynab.py

--- pynab.py
+++ pynab.py
@@ -98,7 +98,7 @@ def disable_group(group):
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(__file__))
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     monitor = config.monitor.get('type', None)
 
