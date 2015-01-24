9d6c6a8b06120e1ffcc24d5197b2c185573dfa4f
AlanKK <alankeister@gmail.com>
2015-01-24 13:28:55 -0500

    auto-find pynab logging_dir

--- tmux/start_tmux.sh
+++ tmux/start_tmux.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# find the pynab logs
+SCRIPT_PATH=$( readlink -m $( type -p $0 ))      # Full path to script
+SCRIPT_DIR=$( dirname ${SCRIPT_PATH} )           # Directory script is run in
+LOGGING_DIR=$( grep logging_dir $SCRIPT_DIR/../config.py | awk -F\' '{print $4 }' )
+
 byobu -2 new-session -d -s pynab
 
 byobu new-window -t pynab:1 -n 'Pynab'
-byobu send-keys "tail -f /var/log/pynab/stats.csv" C-m
-byobu split-window -h -t 0 "tail -f /var/log/pynab/backfill.log"
+byobu send-keys "tail -f $LOGGING_DIR/stats.csv" C-m
 
-byobu split-window -v  -t 1 "tail -f /var/log/pynab/postprocess.log" 
+byobu split-window -h -t 0 "tail -f $LOGGING_DIR/backfill.log"
+byobu split-window -v  -t 1 "tail -f $LOGGING_DIR/postprocess.log" 
+byobu split-window -v  -t 2 "tail -f $LOGGING_DIR/update.log" 
 
-byobu split-window -v  -t 2 "tail -f /var/log/pynab/update.log" 
 byobu select-pane -t 0
 
 # from http://stackoverflow.com/a/22566549
