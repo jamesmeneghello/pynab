4f6c2699d2c3f82c69a02ec7ef1bb752d600b872
James Meneghello <murodese@gmail.com>
2015-01-24 17:58:23 +0800

    add other-misc releases to stats

--- scripts/stats.py
+++ scripts/stats.py
@@ -30,8 +30,10 @@ def get_stats():
         binaries = db.query(Binary).count()
         releases = db.query(Release).count()
         # backlog = db.query(Release).filter(Release.passworded=='UNKNOWN').count()
+        # other-misc releases, ie. hashed, yet-to-be renamed or just bad releases
+        others = db.query(Release).count()
 
-        return parts, binaries, releases
+        return parts, binaries, releases, others
 
 
 def colored(num):
@@ -50,7 +52,7 @@ def build_header():
     """
     Generate a header string.
     """
-    return '{:^21}|{:^21}|{:^21}'.format('Parts', 'Binaries', 'Releases')
+    return '{:^21}|{:^21}|{:^21}|{:^21}'.format('Parts', 'Binaries', 'Releases', 'Other-Misc Releases')
 
 
 if __name__ == '__main__':
@@ -68,28 +70,32 @@ if __name__ == '__main__':
     last_parts = 0
     last_binaries = 0
     last_releases = 0
+    last_others = 0
 
     while True:
-        parts, binaries, releases = get_stats()
+        parts, binaries, releases, others = get_stats()
 
         if not first:
             p_diff = parts - last_parts
             b_diff = binaries - last_binaries
+            o_diff = others - last_others
             r_diff = releases - last_releases
         else:
             first = False
             p_diff = 0
             b_diff = 0
             r_diff = 0
+            o_diff = 0
 
         if i % config.stats.get('header_every_nth', 0) == 0:
             i = 1
             print(build_header())
 
-        print('{:^10} {:^20}|{:^10} {:^20}|{:^10} {:^6}'.format(
+        print('{:^10} {:^10}|{:^10} {:^10}|{:^10} {:^10}|{:^10} {:^10}'.format(
             parts, colored(p_diff),
             binaries, colored(b_diff),
-            releases, colored(r_diff)
+            releases, colored(r_diff),
+            others, colored(o_diff)
         ))
 
         # write to csv file
@@ -97,21 +103,23 @@ if __name__ == '__main__':
             # write the header if we're creating the file
             if not os.path.exists(csv_path):
                 csv = open(csv_path, 'w')
-                csv.write('Date,Parts,part_diff,Binaries,bin_diff,Releases,rel_diff\n')
+                csv.write('Date,Parts,part_diff,Binaries,bin_diff,Releases,rel_diff,Other-Misc Releases,bad_diff\n')
                 csv.close()
 
             csv = open(csv_path, 'a')
-            csv.write('{},{},{},{},{},{},{}\n'.format(
+            csv.write('{},{},{},{},{},{},{},{},{}\n'.format(
                 time.strftime("%Y-%m-%d %H:%M:%S"),
                 parts, p_diff,
                 binaries, b_diff,
-                releases, r_diff
+                releases, r_diff,
+                others, o_diff
             ))
             csv.close()
 
         last_parts = parts
         last_binaries = binaries
         last_releases = releases
+        last_others = others
         i += 1
 
         time.sleep(config.stats.get('sleep_time', 300))
