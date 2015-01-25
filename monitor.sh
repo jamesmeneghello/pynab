7f7f564287751cab0e8f713825349b9728ba93d0
James Meneghello <murodese@gmail.com>
2015-01-25 23:05:04 +0800

    tiniest and yet most annoying error in grep syntax

--- monitor.sh
+++ monitor.sh
@@ -5,7 +5,7 @@
 #
 SCRIPT_PATH=$( readlink -m $( type -p $0 ))      # Full path to script
 SCRIPT_DIR=$( dirname ${SCRIPT_PATH} )           # Directory script is run in
-LOGGING_DIR=$( grep logging_dir $SCRIPT_DIR/config.py | awk -F\' '{print $4 }' )
+LOGGING_DIR=$( grep \'logging_dir\' $SCRIPT_DIR/config.py | awk -F\' '{print $4 }' )
 
 #
 # Find byobu or tmux
