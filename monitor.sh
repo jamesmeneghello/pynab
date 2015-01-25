d11377e53d5cb9bd6232032d4c3c2bf850a4101a
AlanKK <alankeister@gmail.com>
2015-01-25 07:57:06 -0500

    Pane layout tweaks

--- monitor.sh
+++ monitor.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 #
+# discover pynab paths
+#
+SCRIPT_PATH=$( readlink -m $( type -p $0 ))      # Full path to script
+SCRIPT_DIR=$( dirname ${SCRIPT_PATH} )           # Directory script is run in
+LOGGING_DIR=$( grep logging_dir $SCRIPT_DIR/config.py | awk -F\' '{print $4 }' )
+
+#
 # Find byobu or tmux
 #
 BYOBU=$( which byobu )
 
 if [ "$BYOBU" != "" ]  ; then 
     echo "Found byobu"
+    echo
+    echo "Byobu keys:"
+    echo "   Alt-F1             Help / Show all keys"
+    echo "   Alt-Left/Right     move to next/prev window"
+    echo
     CMD=$BYOBU
 else
     TMUX=$( which tmux )
@@ -20,37 +32,36 @@ fi
 
 
 #
-# find the pynab logs
+# MAIN window
 #
-SCRIPT_PATH=$( readlink -m $( type -p $0 ))      # Full path to script
-SCRIPT_DIR=$( dirname ${SCRIPT_PATH} )           # Directory script is run in
-LOGGING_DIR=$( grep logging_dir $SCRIPT_DIR/config.py | awk -F\' '{print $4 }' )
+$CMD -2 new-session -d -s pynab -n 'MAIN' "python3 $SCRIPT_DIR/scripts/stats.py"
+$CMD split-window -h "echo 'ERROR|CRITICAL errors from all logs' ; tail -f $LOGGING_DIR/*.log  | egrep \"ERROR|CRITICAL\""
 
+$CMD resize-pane -t 0 -x 87    # fixed width stats pane won't survive window resize
+$CMD select-pane -t 1
 
 #
-# start the byobu/tmux session
+# LOGS window - four even-horizontal panes
 #
-$CMD -2 new-session -d -s pynab
-$CMD new-window -n 'Pynab'
+$CMD new-window -t 1 -n 'LOGS' "tail -f $LOGGING_DIR/postprocess.log" 
+$CMD split-window -v "tail -f $LOGGING_DIR/update.log" 
+$CMD split-window -v "tail -f $LOGGING_DIR/backfill.log"
+$CMD select-layout even-vertical
+$CMD split-window -h "tail -f $LOGGING_DIR/prebot.log"
+$CMD select-pane -t 0                                   # highlight 
+$CMD split-window -h "tail -f $LOGGING_DIR/scan.log"    # split top window and add this
+$CMD select-pane -t 2                                   # highlight 
 
-$CMD send-keys "python3 $SCRIPT_DIR/scripts/stats.py" C-m
+$CMD select-window -t 'MAIN'
 
-$CMD split-window -h -t 0 "tail -f $LOGGING_DIR/backfill.log"
-
-# set the three panes to take even thirds of the vertical space. Second log pane uses 
-# 66% of first. Next uses 1/2 of it or 33% of total.
-$CMD split-window -v  -t 1 -p 66 "tail -f $LOGGING_DIR/postprocess.log" 
-$CMD split-window -v  -t 2 -p 50 "tail -f $LOGGING_DIR/update.log" 
-
-$CMD select-pane -t 0
-
-$CMD resize-pane -t 0 -x 63    # fixed width stats pane. it won't survive window resize
 $CMD attach-session -t pynab
+
 
 # from http://stackoverflow.com/a/22566549
 #
 # To adjust the layout of panes, if you have the same number as in this script, you can 
-# optionally run "tmux list-windows" and copy the output into the layout line below.
+# optionally run "tmux list-windows" and copy the output into the layout line below rather
+# than messing with tmux layouts, which are clunky.
 #
 # layout=a822,211x52,0,0{63x52,0,0,1,147x52,64,0[147x14,64,0,2,147x18,64,15,3,147x18,64,34,6]}
 # $CMD select-layout "$layout"
