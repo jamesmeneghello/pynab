b5d4f79d2699bf0e24eef36129707a9c40d57400
James Meneghello <murodese@gmail.com>
2015-02-16 22:19:06 +0800

    optimise request matching

--- pynab/requests.py
+++ pynab/requests.py
@@ -1,5 +1,5 @@
 from pynab import log
-from pynab.db import db_session, Release, Pre
+from pynab.db import db_session, Release, Pre, Group
 
 
 GROUP_ALIASES = {
@@ -12,7 +12,7 @@ def process(limit=None):
     """Process releases for requests"""
 
     with db_session() as db:
-        query = db.query(Release).filter(Release.name.like('REQ:%')).filter(Release.pre_id == None).filter(
+        query = db.query(Release).join(Group).filter(Release.name.like('REQ:%')).filter(Release.pre_id == None).filter(
             Release.category_id == '8010')
 
         if limit:
@@ -25,30 +25,34 @@ def process(limit=None):
 
         if releases:
             for release in releases:
-                requests[int(release.name.split(': ')[1])] = release
+                # check if it's aliased
+                if release.group.name in GROUP_ALIASES:
+                    group_name = GROUP_ALIASES[release.group.name]
+                else:
+                    group_name = release.group.name
+
+                if group_name not in requests:
+                    requests[group_name] = {}
+                requests[group_name][int(release.name.split(': ')[1])] = release
         else:
             log.info("requests: no release requests to process")
 
-        # query for the requestids
-        if requests:
-            pres = db.query(Pre).filter(Pre.requestid.in_(requests.keys())).all()
-        else:
-            log.info("requests: no pre requests found")
-            pres = []
+        # per-group
+        for group_name, group_requests in requests.items():
+            # query for the requestids
+            if requests:
+                pres = db.query(Pre).filter(Pre.requestgroup==group_name).filter(Pre.requestid.in_(group_requests.keys())).all()
+            else:
+                log.info("requests: no pre requests found")
+                pres = []
 
-        # loop through and associate pres with their requests
-        for pre in pres:
-            pre_group = requests.get(pre.requestid).group.name
-            if pre_group in GROUP_ALIASES:
-                pre_group = GROUP_ALIASES[pre_group]
-
-            if pre.requestgroup == pre_group:
-                updated_release = requests.get(pre.requestid)
+            # loop through and associate pres with their requests
+            for pre in pres:
+                # no longer need to check group
+                updated_release = group_requests.get(pre.requestid)
                 updated_release.pre_id = pre.id
                 db.merge(updated_release)
-                log.info("requests: found pre request id {} ({}) for {}".format(pre.requestid, pre_group,
+                log.info("requests: found pre request id {} ({}) for {}".format(pre.requestid, group_name,
                                                                                 updated_release.name))
-            else:
-                log.info("requests: no pre request found for {}".format(requests.get(pre.requestid).name))
 
             db.commit()
