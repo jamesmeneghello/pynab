eb7a88700df01a2d6e07f28f6110c908c656b0d7
James Meneghello <murodese@gmail.com>
2015-04-06 12:59:42 +0800

    missed a js file

--- webui/app/scripts/controllers/search.js
+++ webui/app/scripts/controllers/search.js
@@ -3,112 +3,128 @@
 angular.module('pynabWebuiApp')
     .controller('IndexCtrl', function ($scope, $http, PYNAB_CONFIG) {
         var params = {
-			t: 'stats',
-			o: 'json',
-			callback:'JSON_CALLBACK'
-		};
+            t: 'stats',
+            o: 'json',
+            callback: 'JSON_CALLBACK'
+        };
 
-        $http.jsonp(PYNAB_CONFIG.hostUrl + 'api', {params:params}).then(function(response) {
-			var totals = [];
-			response.data.stats.totals.total.forEach(function(total) {
+        $http.jsonp(PYNAB_CONFIG.hostUrl + 'api', {params: params}).then(function (response) {
+            var totals = [];
+            response.data.stats.totals.total.forEach(function (total) {
                 totals.push(total);
-			});
+            });
 
             $scope.totals = totals;
 
             var categories = [];
-            response.data.stats.categories.category.forEach(function(category) {
-				categories.push(category);
-			});
+            response.data.stats.categories.category.forEach(function (category) {
+                categories.push(category);
+            });
 
-			$scope.categories = categories;
-		});
+            $scope.categories = categories;
+
+            var groups = [];
+            response.data.stats.groups.group.forEach(function (group) {
+                groups.push(group);
+            });
+
+            $scope.groups = groups;
+        });
     })
-	.controller('SearchCtrl', function ($scope, $http, $cookieStore, PYNAB_CONFIG) {
-		$scope.searchForm = {};
+    .controller('SearchCtrl', function ($scope, $http, $cookieStore, PYNAB_CONFIG) {
+        $scope.searchForm = {};
 
-		if ($cookieStore.get('apikey')) {
-			$scope.searchForm.apikey = $cookieStore.get('apikey');
-		}
+        if ($cookieStore.get('apikey')) {
+            $scope.searchForm.apikey = $cookieStore.get('apikey');
+        }
 
-		if ($cookieStore.get('remember')) {
-			$scope.searchForm.remember = $cookieStore.get('remember');
-		}
+        if ($cookieStore.get('remember')) {
+            $scope.searchForm.remember = $cookieStore.get('remember');
+        }
 
-		var params = {
-			t: 'caps',
-			o: 'json',
-			callback:'JSON_CALLBACK'
-		};
+        var params = {
+            t: 'caps',
+            o: 'json',
+            callback: 'JSON_CALLBACK'
+        };
 
         $scope.sortOrder = '-posted';
 
-		$http.jsonp(PYNAB_CONFIG.hostUrl + 'api', {params:params}).then(function(response) {
-			var categories = [];
-			response.data.caps.categories.category.forEach(function(category) {
-				if (category.subcat instanceof Array) {
-					category.subcat.forEach(function(subcategory) {
-						subcategory.name = category.name + ' > ' + subcategory.name;
-						categories.push(subcategory);
-					});
-				} else {
-					category.subcat.name = category.name + ' > ' + category.subcat.name;
-					categories.push(category.subcat);
-				}
-			});
-			$scope.categories = categories;
-		});
+        $http.jsonp(PYNAB_CONFIG.hostUrl + 'api', {params: params}).then(function (response) {
+            var categories = [];
+            response.data.caps.categories.category.forEach(function (category) {
+                if (category.subcat instanceof Array) {
+                    category.subcat.forEach(function (subcategory) {
+                        subcategory.name = category.name + ' > ' + subcategory.name;
+                        categories.push(subcategory);
+                    });
+                } else {
+                    category.subcat.name = category.name + ' > ' + category.subcat.name;
+                    categories.push(category.subcat);
+                }
+            });
+            $scope.categories = categories;
+        });
 
-		$scope.search = function() {
-			$scope.searchResults = null;
-			$scope.errorApikey = null;
+        $scope.search = function () {
+            $scope.searchResults = null;
+            $scope.errorApikey = null;
 
-			var params = {
-				t: 'search',
-				o: 'json',
-				limit: 100,
-				cat: $scope.searchForm.cat.join(),
-				q: $scope.searchForm.q,
-				apikey: $scope.searchForm.apikey,
-				callback:'JSON_CALLBACK'
-			};
-			
-			$http.jsonp(PYNAB_CONFIG.hostUrl + 'api', {params:params}).then(function(response) {
-				if ('error' in response.data) {
-					var error = response.data.error;
-					if (error.code === '100') {
-						$scope.errorApikey = error.description;
-					}
-				} else {
-					var results = response.data.rss.channel.item;
+            var params = {
+                t: 'search',
+                o: 'json',
+                limit: 100,
+                cat: $scope.searchForm.cat.join(),
+                q: $scope.searchForm.q,
+                apikey: $scope.searchForm.apikey,
+                callback: 'JSON_CALLBACK'
+            };
+
+            $http.jsonp(PYNAB_CONFIG.hostUrl + 'api', {params: params}).then(function (response) {
+                if ('error' in response.data) {
+                    var error = response.data.error;
+                    if (error.code === '100') {
+                        $scope.errorApikey = error.description;
+                    }
+                } else {
+                    var results = response.data.rss.channel.item;
 
                     if (!(results instanceof Array)) {
                         results = [results];
                     }
 
                     $scope.searchResults = [];
-                    angular.forEach(results, function(obj) {
+                    angular.forEach(results, function (obj) {
                         obj.pubDate = moment(obj.pubDate, "ddd, DD MMM YYYY HH:mm:ss ZZ").toDate();
                         $scope.searchResults.push(obj);
                     });
 
-					if ($scope.searchForm.remember) {
-						$cookieStore.put('remember', true);
-						$cookieStore.put('apikey', $scope.searchForm.apikey);
-					} else {
-						$cookieStore.put('remember', false);
-						$cookieStore.put('apikey', '');
-					}
-				}
-			});
-		};
-	})
-    .filter('bytes', function() {
-        return function(bytes, precision) {
-            if (bytes==0 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) { return '-'; }
-            if (typeof precision === 'undefined') { precision = 1; }
+                    if ($scope.searchForm.remember) {
+                        $cookieStore.put('remember', true);
+                        $cookieStore.put('apikey', $scope.searchForm.apikey);
+                    } else {
+                        $cookieStore.put('remember', false);
+                        $cookieStore.put('apikey', '');
+                    }
+                }
+            });
+        };
+    })
+    .filter('bytes', function () {
+        return function (bytes, precision) {
+            if (bytes == 0 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
+                return '-';
+            }
+            if (typeof precision === 'undefined') {
+                precision = 1;
+            }
             var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
                 number = Math.floor(Math.log(bytes) / Math.log(1024));
-            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
         };
+    })
+    .filter('abs', function () {
+        return function (val) {
+            return Math.abs(val);
+        }
     });
