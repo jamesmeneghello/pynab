183b90670e77376c17cf92aae7982a3f69c92e84
James Meneghello <murodese@gmail.com>
2015-04-11 00:17:01 +0800

    ref #191: change ordinal for regex

--- db/regex.py
+++ db/regex.py
@@ -4,7 +4,7 @@ additions = [
         'id': 200000,
         'group_name': '.*',
         'regex': '/^trtk\d{4,8} - \[\d{1,5}\/\d{1,5}\] - "(?P<name>.+?)\.(?:nzb|vol\d+(?:\+\d+){1,}?\.par2|part\d+\.rar|par2|r\d{1,})" yEnc \(\d{1,5}\/\d{1,5}\)$/i',
-        'ordinal': 2,
+        'ordinal': 1,
         'status': True,
         'description': 'music releases'
     }
