631048858f41a067f8b8e6f0e396a7074735cb5e
James Meneghello <murodese@gmail.com>
2015-04-12 09:15:41 +0800

    ref #191: fix regex

--- db/regex.py
+++ db/regex.py
@@ -3,7 +3,7 @@ additions = [
     {
         'id': 200000,
         'group_name': '.*',
-        'regex': '/^trtk\d{4,8} - \[\d{1,5}\/\d{1,5}\] - "(?P<name>.+?)\.(?:nzb|vol\d+(?:\+\d+){1,}?\.par2|part\d+\.rar|par2|r\d{1,})" yEnc \(\d{1,5}\/\d{1,5}\)$/i',
+        'regex': '/^trtk\d{4,8} - \[\d{1,5}\/\d{1,5}\] - "(?P<name>.+?)\.(?:nzb|vol\d+(?:\+\d+){1,}?\.par2|part\d+\.rar|par2|r\d{1,})" yEnc/i',
         'ordinal': 1,
         'status': True,
         'description': 'music releases'
