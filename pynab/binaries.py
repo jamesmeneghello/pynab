5f14978daa0a5125cf079a353a2ee45519632fa5
James Meneghello <murodese@gmail.com>
2015-11-01 22:13:16 +0800

    don't commit while tired

--- pynab/binaries.py
+++ pynab/binaries.py
@@ -108,7 +108,8 @@ def process():
                     compiled_regex[reg.id] = regex.compile(r, regex_flags)
                 except Exception as e:
                     log.error('binary: broken regex detected. id: {:d}, removing...'.format(reg.id))
-                    db.query(Regex).filter(Regex.id==reg.id).remove()
+                    db.query(Regex).filter(Regex.id==reg.id).delete()
+                    db.commit()
 
             query = db.query(Part).filter(Part.group_name.in_(relevant_groups)).filter(Part.binary_id == None)
             total_parts = query.count()
@@ -132,7 +133,7 @@ def process():
                         result = compiled_regex[reg.id].search(part.subject)
                     except:
                         log.error('binary: broken regex detected. id: {:d}, removing...'.format(reg.id))
-                        db.query(Regex).filter(Regex.id==reg.id).remove()
+                        db.query(Regex).filter(Regex.id==reg.id).delete()
                         continue
 
                     match = result.groupdict() if result else None
