306d59874f8f47ce9c5f05211f0b5bd0c5c717ac
James Meneghello <murodese@gmail.com>
2015-11-01 22:10:44 +0800

    minor fix to regex deletion

--- pynab/binaries.py
+++ pynab/binaries.py
@@ -108,7 +108,7 @@ def process():
                     compiled_regex[reg.id] = regex.compile(r, regex_flags)
                 except Exception as e:
                     log.error('binary: broken regex detected. id: {:d}, removing...'.format(reg.id))
-                    db.query(Regex).filter(reg.id).remove()
+                    db.query(Regex).filter(Regex.id==reg.id).remove()
 
             query = db.query(Part).filter(Part.group_name.in_(relevant_groups)).filter(Part.binary_id == None)
             total_parts = query.count()
@@ -132,7 +132,7 @@ def process():
                         result = compiled_regex[reg.id].search(part.subject)
                     except:
                         log.error('binary: broken regex detected. id: {:d}, removing...'.format(reg.id))
-                        db.query(Regex).filter(reg.id).remove()
+                        db.query(Regex).filter(Regex.id==reg.id).remove()
                         continue
 
                     match = result.groupdict() if result else None
