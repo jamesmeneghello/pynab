a6406f25e1c03494530a467c0ea3c53fbaf76150
James Meneghello <murodese@gmail.com>
2015-12-03 11:31:36 +0800

    fix #230: change REQ: releases to simple numbers, per changes to pre processing

--- pynab/binaries.py
+++ pynab/binaries.py
@@ -149,7 +149,7 @@ def process():
 
                         # fill name if reqid is available
                         if match.get('reqid') and not match.get('name'):
-                            match['name'] = 'REQ: {}'.format(match['reqid'])
+                            match['name'] = '{}'.format(match['reqid'])
 
                         # make sure the regex returns at least some name
                         if not match.get('name'):
